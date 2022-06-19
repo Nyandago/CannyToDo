9f79ffe9988ead8197b12a0ecf21dac214584f5a
Nyandago <paulo@cannybits.com>
2022-06-19 13:43:34 +0300

    Main Activity

--- app/src/main/java/com/cannybits/cannytodo/MainActivity.kt
+++ app/src/main/java/com/cannybits/cannytodo/MainActivity.kt
@@ -2,10 +2,33 @@ package com.cannybits.cannytodo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var todoList = mutableListOf(
+            Todo("Welcome to Cannyville", true),
+                    Todo("Welcome to Cannyville", false),
+                    Todo("I'm new to Cannyville", true),
+                    Todo("Let's go to Cannyville", false),
+                    Todo("I love Cannyville", true),
+                    Todo("Greetings from Cannyville", false),
+                    Todo("Cannyville is Amazing", false),
+        )
+
+        val adapter = TodoAdapter(todoList)
+        rvTodos.adapter = adapter
+        rvTodos.layoutManager = LinearLayoutManager(this)
+
+        btnAddTodo.setOnClickListener {
+            val title = etTodo.text.toString()
+            val todo = Todo(title, false)
+            todoList.add(todo)
+            adapter.notifyItemInserted(todoList.size - 1)
+        }
     }
 }
